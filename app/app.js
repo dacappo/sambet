0b59ada1f927f49507fbb09b9d54c37e5c879072
Lukas Carullo <lukas.carullo@gmail.com>
2014-05-24 22:03:43 +0200

    First Node App

---
+++ app/app.js
@@ -0,0 +1,93 @@
+/**
+ * Created by Lukas on 24.05.2014.
+ */
+
+var restify = require('restify');
+
+var ip_addr = '127.0.0.1';
+var port    =  '8080';
+
+var server = restify.createServer({
+    name : "myapp"
+});
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+server.listen(port ,ip_addr, function(){
+    console.log('%s listening at %s ', server.name , server.url);
+});
+
+var PATH = '/users';
+server.get({path : PATH , version : '0.0.1'} , findAllUsers);
+server.get({path : PATH +'/:userId' , version : '0.0.1'} , findUser);
+server.post({path : PATH , version: '0.0.1'} ,postNewUser);
+server.del({path : PATH +'/:userId' , version: '0.0.1'} ,deleteUser);
+
+function findAllUsers(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    jobs.find().limit(20).sort({postedOn : -1} , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(200 , success);
+            return next();
+        }else{
+            return next(err);
+        }
+
+    });
+
+}
+
+function findUser(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    jobs.findOne({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(200 , success);
+            return next();
+        }
+        return next(err);
+    })
+}
+
+function postNewUser(req , res , next){
+    var job = {};
+    job.title = req.params.title;
+    job.description = req.params.description;
+    job.location = req.params.location;
+    job.postedOn = new Date();
+
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    jobs.save(job , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(201 , job);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
+}
+
+function deleteUser(req , res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    jobs.remove({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
+        console.log('Response success '+success);
+        console.log('Response error '+err);
+        if(success){
+            res.send(204);
+            return next();
+        } else{
+            return next(err);
+        }
+    })
+
+}
+
