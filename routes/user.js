3e304177897a731c9c6395093c3c75ba1b975b01
Lukas Carullo <lukas@carullo.de>
2014-05-25 15:28:41 +0200

    username not set?

--- routes/user.js
+++ routes/user.js
@@ -21,3 +21,50 @@ exports.create = function(req, res) {
             }
         })
 }
+
+function findAllUsers(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log("Not yet implemented!");
+}
+
+function findUser(req, res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log(req.params.username);
+
+    db.User
+        .find({ where: { username: req.params.username} })
+        .complete(function(err, user) {
+            if (!!err) {
+                console.log('An error occurred while searching for the user:', err)
+            } else if (!user) {
+                console.log('No user has been found.')
+            } else {
+                console.log('Hello ' + user.username + '!')
+                console.log('All attributes of john:', user.values)
+            }
+        })
+}
+
+function postNewUser(req , res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    db.User
+        .create({
+            username: req.params.username,
+            email: req.params.email
+        })
+        .complete(function(err, user) {
+            if (!!err) {
+                console.log('The instance has not been saved:', err)
+                res.send({message: "error"});
+            } else {
+                console.log('We have a persisted instance now')
+                res.send({message: "User created!"});
+            }
+        })
+
+}
+
+function deleteUser(req , res , next){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    console.log("Not yet implemented!");
+}
